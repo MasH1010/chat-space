540318f2560e7cc505a5ada8217e2e960d4cd47c
MasH1010 <cis.trans.relations@gmail.com>
2019-06-22 17:09:04 +0900

    データベース設計修正に伴うモデルの修正
    【追加】
    chatsテーブルにuser_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190619124304) do
+ActiveRecord::Schema.define(version: 20190622075701) do
 
   create_table "chats", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20190619124304) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "group_id"
+    t.integer  "user_id"
   end
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
